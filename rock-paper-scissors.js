3499359b4ac7a889a626516a13e9520b2d4e4568
batoru <tbakanas2@gmail.com>
2022-09-22 22:13:06 +0300

    rock-paper-scissors.html

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,1 +1,18 @@
-function getComputerChoice('Rock' 'Paper' 'Scissors')
+
+
+
+
+    function getComputerChoice() {
+        let pcChoice = ["Rock", "Paper", "Scissors"] /* answers to pick from*/
+        let randomPcChoice = pcChoice[Math.floor(Math.random() * pcChoice.length)] /*randomize pc choice*/
+        return randomPcChoice;
+        
+    }
+    console.log(getComputerChoice()); 
+
+   
+
+ 
+    
+
+
