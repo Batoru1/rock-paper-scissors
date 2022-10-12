a3c90dd590aaac7345681f5b86a429a3493762f1
batoru <tbakanas2@gmail.com>
2022-10-12 22:30:31 +0300

    rock-paper-scissors.js

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -3,51 +3,56 @@
 
 
     function getComputerChoice() {
-        let pcChoice = ['rock', 'paper', 'scissors'] /* answers to pick from*/
-        let randomPcChoice = Math.floor(Math.random() * pcChoice.length)+1 /*randomize pc choice*/
-       
-        if (randomPcChoice === 1 ) {
-            return 'rock';
-        }  else if (randomPcChoice === 2) {
-            return 'paper';
-        } else if (randomPcChoice === 3) {
-            return 'scissors';   
-        }   /* make console.log display either Rock Paper or Scissors */   
-    }
-     /*console.logs either Rock Paper or Scissors from getComputerChoice function*/
+        const pcOptions = ['rock', 'paper', 'scissors'] /* answers to pick from*/
+        const randomNum = Math.floor(Math.random() * pcOptions.length) /*randomize pc choice*/
+        const pcChoice=pcOptions[randomNum]
+       return pcChoice
+     }
+
+    console.log(getComputerChoice()) /*checking computer choice*/
+    
 
     function playRound(playerSelection, computerSelection) {
 
                 
         if (playerSelection === computerSelection) {
-            return `it's a tie! You both chose ${playerSelection.charAt(0).toUpperCase()+ playerSelection.slice(1)}` /*capitalize first letter*/
+            return `it's a tie! You both chose ${playerSelection}` /*template literal's tag function*/
+        
         } else if (playerSelection === 'rock' && computerSelection === 'scissors') {
             return "You win! Rock beats Scissors"
         } else if (playerSelection === 'paper' && computerSelection === 'rock') {
             return "You win! Paper beats Rock"
         } else if (playerSelection === 'scissors' && computerSelection === 'paper') {
             return "You win! Scissors beats Paper"
+       
         } else if (playerSelection === 'scissors' && computerSelection === 'rock') {
-            return `You lose! ${computerSelection.charAt(0).toUpperCase()+ computerSelection.slice(1)} beats ${playerSelection.charAt(0).toUpperCase()+ playerSelection.slice(1)}`;
+            return "You lose! Rock beats Scissors";
         } else if (playerSelection === 'rock' && computerSelection === 'paper') {
-            return `You lose! ${computerSelection.charAt(0).toUpperCase()+ computerSelection.slice(1)} beats ${playerSelection.charAt(0).toUpperCase()+ playerSelection.slice(1)}`;
+            return "You lose! Paper beats Rock";
         } else if (playerSelection === 'paper' && computerSelection === 'scissors') {
-            return `You lose! ${computerSelection.charAt(0).toUpperCase()+ computerSelection.slice(1)} beats ${playerSelection.charAt(0).toUpperCase()+ playerSelection.slice(1)}`;
+            return 'You lose! Scissors beats Paper';
         } else {
+        
             return "TYPO"
         }
         }
 
+        
+        
+        
+
     const playerSelection = prompt("Choose either Rock, Paper or Scissors").toLowerCase();/*makes text case insensitive?*/
     const computerSelection = getComputerChoice();
-     
-    console.log(computerSelection);
+
     console.log(playRound(playerSelection, computerSelection)); /* console.logs the playRound function */
 
-function game() {
-    for (let i = 0; i< 5; i++){
-       playRound (playerSelection, computerSelection)
-    }
+function game(playRound) {
+  
+   
+
+  
+
+    
 }
  
     
