248fbb5dcc9557a396b2002947780dbfcc9949ab
batoru <tbakanas2@gmail.com>
2022-10-17 21:40:17 +0300

    rock-paper-scissors.js

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -3,9 +3,9 @@
 
     function getComputerChoice() {
         const pcOptions = ['rock', 'paper', 'scissors']; /* answers to pick from*/
-        const randomNum = Math.floor(Math.random() * pcOptions.length); /*randomize pc choice to 0, 1, 2(could be 3 instead of .length*/
-        const pcChoice=pcOptions[randomNum]; /* assigns 0, 1, or 2 to 'rock', 'paper' or 'scissors'*/
-       return pcChoice;
+        const randomNum = Math.floor(Math.random() * pcOptions.length); /*randomize pc choice to 0, 1, 2, (could be 3 instead of .length*/
+        return pcOptions[randomNum]; /* assigns 0, 1, or 2 to 'rock', 'paper' or 'scissors'*/
+        
      }
 
                                              /*(console.log(getComputerChoice)) - checking getComputerChoice*/
@@ -14,12 +14,12 @@
     function playRound(playerSelection, computerSelection) { /* plays one round of rps*/
 
         if (playerSelection === computerSelection) {
-            return `it's a tie! You both chose ${playerSelection}`; /*template literal's tag function(it could be ${computerSelection} as well*/
+            return `it's a tie! You both chose ${playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1)}`; /*template literal's tag function(it could be ${computerSelection} as well + first letter always to upper case*/
 
          
         } else if (playerSelection === 'rock' && computerSelection === 'scissors') { /* playerScore++ === playerScore=playerScore+1*/
             playerScore++;
-            return "You win! Rock beats Scissors"
+            return "You win! Rock beats Scissors";
          } else if (playerSelection === 'paper' && computerSelection === 'rock') {
             playerScore++;
             return "You win! Paper beats Rock";
@@ -38,19 +38,20 @@
             compScore++;
             return 'You lose! Scissors beats Paper';
 
-        } else {
+        } else  { 
         
-            return "TYPO";
+            return "TYPO - choose wisely!";
         }
         }
 
-            
+         
+        
+
         let playerScore = 0;
         let compScore = 0;/* for keeping score in game(), let so the score can change */
-    
         
         function game() {
-        for (let i = 0; i < 5; i++) {
+        for (let i = 0; i < 5; i++) { /*to play 5 rounds*/
             const playerSelection = prompt("Choose either Rock, Paper or Scissors", 'Rock, Paper, Scissors').toLowerCase();/*makes text case insensitive?*/
             const computerSelection = getComputerChoice();
             playRound(playerSelection, computerSelection); 
@@ -61,9 +62,9 @@
                 return 'You rock! You beat a computer!'
              } else if (playerScore<compScore) {
                 return 'You lose! But you still rock!'
-             } else {
+             } else  {
                 return 'You both rock! It\'s a tie'
-             }
+             } 
     }
 
     
